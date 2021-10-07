405f857feb55f9ec743fc059a73817866f73eb2a
kimjisu33 <81640695+kimjisu33@users.noreply.github.com>
2021-10-07 12:03:22 +0900

    Create 부족한 금액 계산하기.c

---
+++ 부족한 금액 계산하기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int price, int money, int count) {
+    long long answer = 0;
+    
+    for(int i=1 ; i<=count ; i++){
+        answer+=price*i;
+    }
+    
+    if(answer<=money) return 0;
+    
+    return answer-money;
+}
