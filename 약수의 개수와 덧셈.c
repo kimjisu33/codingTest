26c182b928c1409fe6bbda707c37d781525b9c77
kimjisu33 <81640695+kimjisu33@users.noreply.github.com>
2021-10-07 12:02:10 +0900

    Create 약수의 개수와 덧셈.c

---
+++ 약수의 개수와 덧셈.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int left, int right) {
+    int answer = 0;
+    
+    for(int i=left ; i<=right ; i++){
+        int temp=0;
+        for(int j=1 ; j<=i ; j++){
+            if(i%j==0)temp++;
+        }
+        if(temp%2==0)answer+=i;
+        else answer-=i;
+    }
+    
+    return answer;
+}
