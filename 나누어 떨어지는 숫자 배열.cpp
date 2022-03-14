208b18ba40afdf3791010241de983a82d59ef23d
kimjisu33 <81640695+kimjisu33@users.noreply.github.com>
2022-03-14 22:27:06 +0900

    Create 나누어 떨어지는 숫자 배열.cpp

---
+++ 나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    
+    for(auto e : arr){
+        if(e%divisor==0) answer.push_back(e);
+    }
+    
+    if(answer.size()==0) answer.push_back(-1);
+    else  sort(answer.begin(), answer.end());
+    
+    return answer;
+}
